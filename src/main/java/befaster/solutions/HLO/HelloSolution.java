b401c273d896e42d7a051439fb82cc030c30e25e
sbx_user1051 <sbx_user1051@169.254.20.221>
2024-06-15 09:17:18 +0000

    Sat Jun 15 09:17:18 UTC 2024

--- src/main/java/befaster/solutions/HLO/HelloSolution.java
+++ src/main/java/befaster/solutions/HLO/HelloSolution.java
@@ -4,11 +4,8 @@ import befaster.runner.SolutionNotImplementedException;
 
 public class HelloSolution {
     public String hello(String friendName) {
-//        throw new SolutionNotImplementedException();
         StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append(stringBuilder).append("Hello, World!");
+        stringBuilder.append(stringBuilder).append("Hello, ").append(friendName).append("!");
         return stringBuilder.toString();
     }
 }
-
-
