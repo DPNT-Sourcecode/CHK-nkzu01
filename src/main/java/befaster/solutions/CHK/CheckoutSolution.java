01ae1c09a79077220abaf0f84a02ed8d4984d20c
sbx_user1051 <sbx_user1051@169.254.20.221>
2024-06-15 10:50:51 +0000

    Sat Jun 15 10:50:51 UTC 2024

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -1,5 +1,6 @@
 package befaster.solutions.CHK;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -14,11 +15,16 @@ public class CheckoutSolution {
         Integer nr_E_prom = 2;
         Integer nr_F_prom = 3;
 
+        ArrayList<Product> products = new ArrayList<>(100);
 
         for (int i = 0; i < skus.length(); i++) {
             Character ch = skus.charAt(i);
             if (!freq.containsKey(ch)) {
                 freq.put(ch, 0);
+                if (ch.equals('A')) {
+                    ProductA productA = new ProductA();
+                    products.add(productA);
+                }
             }
             Integer val = freq.get(ch);
             freq.put(ch, val + 1);
@@ -38,19 +44,49 @@ public class CheckoutSolution {
         }
 
 
-
+//        +------+-------+------------------------+
+//                | Item | Price | Special offers         |
+//                +------+-------+------------------------+
+//                | A    | 50    | 3A for 130, 5A for 200 |
+//| B    | 30    | 2B for 45              |
+//| C    | 20    |                        |
+//| D    | 15    |                        |
+//| E    | 40    | 2E get one B free      |
+//| F    | 10    | 2F get one F free      |
+//| G    | 20    |                        |
+//| H    | 10    | 5H for 45, 10H for 80  |
+//| I    | 35    |                        |
+//| J    | 60    |                        |
+//| K    | 80    | 2K for 150             |
+//| L    | 90    |                        |
+//| M    | 15    |                        |
+//| N    | 40    | 3N get one M free      |
+//| O    | 10    |                        |
+//| P    | 50    | 5P for 200             |
+//| Q    | 30    | 3Q for 80              |
+//| R    | 50    | 3R get one Q free      |
+//| S    | 30    |                        |
+//| T    | 20    |                        |
+//| U    | 40    | 3U get one U free      |
+//| V    | 50    | 2V for 90, 3V for 130  |
+//| W    | 20    |                        |
+//| X    | 90    |                        |
+//| Y    | 10    |                        |
+//| Z    | 50    |                        |
+//        +------+-------+------------------------+
         for (Map.Entry<Character, Integer> entry : freq.entrySet()) {
             Character key = entry.getKey();
             Integer val = entry.getValue();
             if (key.equals('A')) {
-//                first promotion
-                Integer nr_promotions1 = val / nr_A_prom_1;
-                totalValue += 200 * nr_promotions1;
-                val -= nr_promotions1 * nr_A_prom_1;
-//                second promotion
-                Integer nr_promotions2 = val / nr_A_prom_2;
-                totalValue += 130 * nr_promotions2;
-                totalValue += (val % nr_A_prom_2) * 50;
+////                first promotion
+//                Integer nr_promotions1 = val / nr_A_prom_1;
+//                totalValue += 200 * nr_promotions1;
+//                val -= nr_promotions1 * nr_A_prom_1;
+////                second promotion
+//                Integer nr_promotions2 = val / nr_A_prom_2;
+//                totalValue += 130 * nr_promotions2;
+//                totalValue += (val % nr_A_prom_2) * 50;
+                totalValue += TODO;
             } else if (key.equals('B')) {
                 Integer nr_promotions = val / nr_B_prom;
                 totalValue += 45 * nr_promotions;
@@ -74,4 +110,6 @@ public class CheckoutSolution {
 
         return totalValue;
     }
+
 }
+
--- src/main/java/befaster/solutions/CHK/Product.java
+++ src/main/java/befaster/solutions/CHK/Product.java
@@ -1,2 +1,31 @@
-package befaster.solutions.CHK;public class Product {
+package befaster.solutions.CHK;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public abstract class Product {
+    int price;
+    int offerPrice1;
+    int offerPrice2;
+    int offerQuantity1;
+    int offerQuantity2;
+    int quantity;
+
+    public Integer calculateDoublePromotion() {
+        Integer total = 0;
+//                first promotion
+        Integer nr_promotions1 = quantity / offerQuantity1;
+        total += offerPrice1 * nr_promotions1;
+        quantity -= nr_promotions1 * offerQuantity1;
+        //                second promotion
+        Integer nr_promotions2 = quantity / offerQuantity2;
+        total += offerPrice2 * nr_promotions2;
+        total += (quantity % offerQuantity2) * price;
+        return total;
+    }
 }
+
