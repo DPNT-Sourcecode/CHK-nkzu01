ee0e2de07fb6e9956186438721eeab59d6094647
sbx_user1051 <sbx_user1051@169.254.20.221>
2024-06-15 09:38:44 +0000

    Sat Jun 15 09:38:44 UTC 2024

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -4,18 +4,36 @@ import befaster.runner.SolutionNotImplementedException;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 public class CheckoutSolution {
     public Integer checkout(String skus) {
 //        throw new SolutionNotImplementedException();
-        HashMap<String, Integer> freq = new HashMap<>();
+        HashMap<Character, Integer> freq = new HashMap<>(4, 0);
+        Integer totalValue = 0;
 
         for (int i = 0; i < skus.length(); i++) {
-            if ( == 'A') {
+            Character ch = skus.charAt(i);
+            Integer val = freq.get(ch);
+            freq.put(ch, val + 1);
+        }
 
+        for (Map.Entry<Character, Integer> entry : freq.entrySet()) {
+            Character key = entry.getKey();
+            if (key.equals('A')) {
+                totalValue += 50;
+            } else if (key.equals('B')) {
+                totalValue += 30;
+            } else if (key.equals('C')) {
+                totalValue += 20;
+            } else if (key.equals('D')) {
+                totalValue += 15;
             }
         }
+
+        return totalValue;
     }
 }
+
 
 
