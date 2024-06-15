9f306659824952f840f7e40589abdee9b00217ec
sbx_user1051 <sbx_user1051@169.254.20.221>
2024-06-15 09:26:44 +0000

    Sat Jun 15 09:26:44 UTC 2024

--- src/test/java/befaster/solutions/CHK/CheckoutTest.java
+++ src/test/java/befaster/solutions/CHK/CheckoutTest.java
@@ -1,2 +1,33 @@
-package befaster.solutions.CHK;public class CheckoutTest {
+package befaster.solutions.CHK;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+public class CheckoutTest {
+    private CheckoutSolution chk;
+
+    @BeforeEach
+    public void setUp() {
+        chk = new CheckoutSolution();
+    }
+
+
+//    Our price table and offers:
+//            +------+-------+----------------+
+//            | Item | Price | Special offers |
+//            +------+-------+----------------+
+//            | A    | 50    | 3A for 130     |
+//            | B    | 30    | 2B for 45      |
+//            | C    | 20    |                |
+//            | D    | 15    |                |
+//            +------+-------+----------------+
+    @Test
+    public void compute_sum() {
+        assertThat(chk.checkout("ABC"), equalTo(100));
+    }
 }
+
