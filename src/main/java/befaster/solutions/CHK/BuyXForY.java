5af2bed27281625ac1bc4ab716c6215e8aec3151
sbx_user1051 <sbx_user1051@169.254.20.221>
2024-06-15 11:17:51 +0000

    Sat Jun 15 11:17:51 UTC 2024

--- src/main/java/befaster/solutions/CHK/BuyXForY.java
+++ src/main/java/befaster/solutions/CHK/BuyXForY.java
@@ -1,2 +1,33 @@
-package befaster.solutions.CHK;public class BuyXForY {
+package befaster.solutions.CHK;
+
+public class BuyXForY {
+    Integer offerQuantity;
+    Integer offerPrice;
+
+    public BuyXForY(Integer offerQuantity, Integer offerPrice) {
+        this.offerQuantity = offerQuantity;
+        this.offerPrice = offerPrice;
+    }
+
+    public Integer getOfferQuantity() {
+        return offerQuantity;
+    }
+
+    public void setOfferQuantity(Integer offerQuantity) {
+        this.offerQuantity = offerQuantity;
+    }
+
+    public Integer getOfferPrice() {
+        return offerPrice;
+    }
+
+    public void setOfferPrice(Integer offerPrice) {
+        this.offerPrice = offerPrice;
+    }
+
+    public Integer calculateOffer(Product product) {
+        Integer nr_promotions = product.getQuantity() / offerQuantity;
+        return nr_promotions * offerPrice;
+    }
 }
+
--- src/main/java/befaster/solutions/CHK/Product.java
+++ src/main/java/befaster/solutions/CHK/Product.java
@@ -1,2 +1,41 @@
-package befaster.solutions.CHK;public class Product {
+package befaster.solutions.CHK;
+
+public class Product {
+    String name;
+    Integer price;
+    Integer quantity;
+
+    public Product() {
+    }
+
+    public Product(String name, Integer price, Integer quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
 }
+
